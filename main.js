170d52e9339c9a753ead78cf8c553039b7ed181c
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-05-16 12:38:15 -0600

    feat:add exercise solution

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ const countries = [
         language: "German"
     }
 ]
+countries.forEach(element => console.log(element.capital + " is the capital of " + element.country + ". People speak " + element.language + " there. "));
+
+
+
 
 //ваша задача вывести в консоль фразы:
 // {capital} is the capital of {coutry}. People speak {language} there
@@ -27,6 +31,11 @@ const countries = [
 //2 Вы работаете с массивом зарплат:
 
 const salaries = [300, 500, 1800, 120, 750, 3200, 1310];
+console.log(salaries.sort((a, b) => b - a));
+let newSalaries = salaries.map(element => element + 200);
+console.log(newSalaries);
+let superNewSalaries = newSalaries.filter(element => element > 1000);
+console.log(superNewSalaries);
 
 //отсортируйте его в порядке убывания 
 //затем создайте новый массив на основе массива salaries, в котором все зарплаты поднимутеся на 200.
@@ -38,6 +47,20 @@ const salaries = [300, 500, 1800, 120, 750, 3200, 1310];
 //3 У вас есть строковая переменная
 
 let phrase = "My favorite color is green";
+let newPhrase = phrase.split(" ");
+//console.log(newPhrase);
+newPhrase.pop();//delete the last element
+//console.log(newPhrase);
+newPhrase.push("purple"); //push the last element
+//console.log(newPhrase);
+let superPhrase = newPhrase.join(" ");
+console.log(superPhrase);//result
+
+
+
+
+
+
 
 //ваша задача заменить слово green на purple. На выходе у вас должна получиться фраза 
 //"My favorite color is purple" . Попробуйте преобразовать строку в массив, заменить 4й элемент и снова объединить в строку.
@@ -45,10 +68,13 @@ let phrase = "My favorite color is green";
 //4 Отсортируйте список работников по алфавиту
 
 const workers = ["Stone", "Goldberg", "House", "Andersen", "Black", "Swan", "McKenzy", "Hamilton"];
+console.log(workers.sort());
 
 //5 Объедините 2 массива workers и bosses, отсортируйте их по алфавиту.
 
 const bosses = ["Potter", "Baley", "Hudson", "Green"];
+let newArr = workers.concat(bosses).sort();
+console.log(newArr);
 
 //попробуйте сделать это , зачейнив методы (т.е. написав их по порядку через точку например arr.sort().reverse())
 
@@ -56,7 +82,8 @@ const bosses = ["Potter", "Baley", "Hudson", "Green"];
 //условие - все элементы массива кратны 5
 
 function checkArray(array) {
-    //ваш код
+    console.log(array.every(element => element % 5 === 0));
+
 }
 
 checkArray([5, 10, 15, 20, 100]) //true
